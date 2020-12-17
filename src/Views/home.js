4bd32190b5d2c4819cdcf94fefb74794311a8ab7
Akbar <akbar.h@samsung.com>
2020-12-17 16:41:55 +0530

    Form added

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,17 +1,97 @@
 import React, { Component } from "react";
 import CardCompact from '../Components/cardCompact';
-
+import { Row, Col, Form } from 'react-bootstrap'
+import { TextField } from '@material-ui/core';
+import Button from '../Components/button';
+import '../style.css';
 class Home extends Component {
   render() {
     let container = (
-      <CardCompact
-        bigIcon={<i className="fa fa-clock-o text-warning" />}
-        statsText="Res. Time"
-        statsValue={100}
-      />)
+      <Row>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+        <Col>
+          <CardCompact
+            bigIcon={<i className="fa fa-clock-o text-warning" />}
+            statsText="----"
+            statsValue="-"
+          />
+        </Col>
+      </Row>
+    )
 
-    
-    return <h6>Tracer Samsung</h6>
+    let form = (
+      <Row>
+        <Col sm="12" lg="4" md="5">
+          <div className="card">
+            <div className="content">
+              <Form name="individualTraceForm">
+                <Row>
+                  <Col className="u_form">
+                    <TextField
+                      id="outlined-textarea"
+                      name="question"
+                      label="Search Order"
+                      placeholder="We trace order for you"
+                      variant="outlined"
+                      value=''
+                      fullWidth
+                      required
+                      size="small"
+                    // onChange={e => this.handleChange(e, 0)}
+                    />
+                  </Col>
+                  <Col className="u_button">
+                    <Button />
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    )
+
+    return (
+      <div>
+        <br />
+        {form}
+        {/* {container} */}
+      </div>
+    )
   }
 }
 
